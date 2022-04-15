b3ef59ba4586875230531fa2a982684afe6109e8
jaydev-growexxer <jaydev.vadachhak@growexx.com>
2022-04-15 16:37:27 +0530

    Login & Register UI Form Complete With Validation (Except Password Pattern Validation)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginUserComponent,
+  },
+  {
+    path: 'userRegister',
+    component: RegisterUserComponent,
+  },
+  {
+    path: 'userLogin',
+    component: LoginUserComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
